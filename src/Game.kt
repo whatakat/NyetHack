204d1e59b85394767388cd6c71e72404325a9b24
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-08-15 15:14:22 +0300

    Initial commit

---
+++ src/Game.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>) {
+    val name = "Magrigal"
+    var healthPoints = 100
+    if (healthPoints==100){
+        println(name + " is in excelent condition!")
+    }else{
+        println(name + " is in awful condition!")
+    }
+
+}
