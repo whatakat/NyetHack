f8ae9801be305cbab5e72589cccdab4e3fb6b641
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-08-17 23:19:40 +0300

    when conditional expression

--- src/Game.kt
+++ src/Game.kt
@@ -1,10 +1,23 @@
 fun main(args:Array<String>) {
     val name = "Magrigal"
-    var healthPoints = 100
-    if (healthPoints==100){
-        println(name + " is in excelent condition!")
-    }else{
-        println(name + " is in awful condition!")
+    var healthPoints = 89
+    val isBlassed = true
+    val isImmortal = false
+    val auraVisible = isBlassed&&healthPoints>50||isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+
+    val healthStatus = when(healthPoints){
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlassed){
+            "has some minor wounds but is healing quite quickly!"
+        }else{
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
+    println("(Aura: $auraColor) "+ "(Blessed: ${if (isBlassed) "YES" else "NO"})")
+    println("$name $healthStatus")
 
 }
