380a0ff9582635fa005542ee854b6d2db8be0850
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-09-08 23:03:01 +0300

    while

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -29,6 +29,11 @@ fun main(args: Array<String>) {
         uniquePatrons +=name
     }
     println(uniquePatrons)
+    var orderCount = 0
+    while (orderCount<=9){
+        placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
+        orderCount++
+    }
 }
 fun performPurchase(price: Double){
     displayBalance()
