78588962158a09115c7138dac7739e5ee3f12b5e
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-09-25 19:53:25 +0300

    add class TownSquare

--- src/com/bankmtk/nyethack/Room.kt
+++ src/com/bankmtk/nyethack/Room.kt
@@ -1,6 +1,14 @@
 package com.bankmtk.nyethack
 
-class Room(val name: String){
-    fun descroption() = "Room: $name"
-    fun load() = "Nothing much to see here..."
+open class Room(val name: String){
+    protected open val dangerLevel = 5
+    fun descroption() = "Room: $name\n"+
+            "Danger level: $dangerLevel"
+    open fun load() = "Nothing much to see here..."
+}
+class TownSquare : Room("Town Square"){
+    override val dangerLevel = super.dangerLevel - 3
+    private var bellSound ="GWONG"
+    override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
+    private fun ringBell() = "The bell tower announces your arrival. $bellSound"
 }
