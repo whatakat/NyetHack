0d0c39e7a9bf5d4cfb578f97a2ee4d5be8448766
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-09-27 22:36:15 +0300

    add class GameInput

--- src/com/bankmtk/nyethack/Game.kt
+++ src/com/bankmtk/nyethack/Game.kt
@@ -25,6 +25,13 @@ object Game{
         println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlassed) "YES" else "NO"})")
         println("${player.name} ${player.formatHealthStatus()}")
     }
+
+    private class GameInput(arg: String?){
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1,{ "" })
+
+    }
 }
 
 
