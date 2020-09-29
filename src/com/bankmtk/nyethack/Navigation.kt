769621e02439539e5631644e026ba378bb949e10
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-09-29 21:45:03 +0300

    operator fun plus

--- src/com/bankmtk/nyethack/Navigation.kt
+++ src/com/bankmtk/nyethack/Navigation.kt
@@ -1,7 +1,9 @@
 package com.bankmtk.nyethack
 
 data class Coordinate(val x: Int, val y: Int){
-    val isInBonds = x >=0 && y >= 0 }
+    val isInBonds = x >=0 && y >= 0
+operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
+}
 enum class Direction(private val coordinate: Coordinate){
     NORTH(Coordinate(0,-1)),
     EAST(Coordinate(1,0)),
@@ -9,6 +11,5 @@ enum class Direction(private val coordinate: Coordinate){
     WEST(Coordinate(-1,0));
 
     fun updateCoordinate(playerCoordinate: Coordinate) =
-        Coordinate(playerCoordinate.x  + coordinate.x,
-        playerCoordinate.y + coordinate.y)
+        coordinate + playerCoordinate
 }
