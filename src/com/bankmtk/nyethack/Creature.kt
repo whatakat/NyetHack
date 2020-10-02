bd15474b082d35e86779e8ab4dc6299d72a7a175
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-10-02 23:54:10 +0300

    add class Goblin

--- src/com/bankmtk/nyethack/Creature.kt
+++ src/com/bankmtk/nyethack/Creature.kt
@@ -19,3 +19,8 @@ abstract class Monster(val name: String, val description: String, override var h
         return damageDealt
     }
 }
+class Goblin(name: String = "Goblin", description: String = "A nasty-looking goblin",
+             healthPoints: Int = 30): Monster(name,description, healthPoints){
+    override val diceCount: Int= 2
+    override val diceSides: Int= 8
+}
