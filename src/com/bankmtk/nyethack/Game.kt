6a5b015ff70a0857b3f986d917ec33ac8c525d39
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-10-03 22:29:31 +0300

    add fun fight

--- src/com/bankmtk/nyethack/Game.kt
+++ src/com/bankmtk/nyethack/Game.kt
@@ -58,6 +58,12 @@ object Game{
         } catch (e: Exception){
             "Invalid direction: $directionInput."
         }
+    private fun fight() = currentRoom.monster?.let {
+        while (player.healthPoints> 0 && it.healthPoints>0){
+            Thread.sleep(1000)
+        }
+        "Combat complete."
+    } ?: "There's nothing here to fight."
 }
 
 
